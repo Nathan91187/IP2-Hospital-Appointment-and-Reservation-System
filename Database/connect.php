818d855ff9a41b453c08a1045210abfff6b6ea24
ansuar5254 <ansuar5254@gmail.com>
2026-05-17 14:24:32 +0300

    edit the connection file

--- Database/connect.php
+++ Database/connect.php
@@ -1,10 +1,16 @@
 <?php
-$host = 'ep-spring-cherry-aov76suf.c-2.ap-southeast-1.aws.neon.tech';
-$port = '5432';
-$dbname = 'neondb';
-$user = 'neondb_owner';
-$password = 'npg_3uVZ8KwHmhdj';
-
-$conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require", $user, $password);
-echo "Connected to Neon!";
+try {
+    $dsn = "pgsql:host=ep-spring-cherry-aov76suf.c-2.ap-southeast-1.aws.neon.tech;
+            port=5432;
+            dbname=neondb;
+            sslmode=require;
+            options=endpoint=ep-spring-cherry-aov76suf";
+    $user = "neondb_owner";
+    $password = "npg_3uVZ8KwHmhdj";
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
